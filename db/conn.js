4646b5bdf15e57fb2b8c98034554e075b0a20619
khankalem-4275 <khankalem.4275@gmail.com>
2024-08-18 00:01:27 +0530

    updates to db_connection

--- db/conn.js
+++ db/conn.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const client = mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .then(() => {
     console.log("DB connected");
@@ -17,3 +18,4 @@ const client = mongoose
   });
 
 module.exports = client;
+
